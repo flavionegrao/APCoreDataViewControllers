cda47bfbfcb17a0b66b8768adefc83d0d58389af
flavionegrao <flavio@apetis.com>
2015-06-18 12:09:41 -0300

    iPad splitscreen

--- APCoreDataViewControllers/APCoreDataTVC.m
+++ APCoreDataViewControllers/APCoreDataTVC.m
@@ -180,20 +180,20 @@ NSString* const APCoreDataTVCNotificationRefresh = @"com.apetis.apChatController
             switch(type) {
                     
                 case NSFetchedResultsChangeInsert:
-                    [self.tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+                    [self.tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
                     break;
                     
                 case NSFetchedResultsChangeDelete:
-                    [self.tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+                    [self.tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
                     break;
                     
                 case NSFetchedResultsChangeUpdate:
-                    [self.tableView reloadRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+                    [self.tableView reloadRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
                     break;
                     
                 case NSFetchedResultsChangeMove:
-                    [self.tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
-                    [self.tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+                    [self.tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+                    [self.tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
                     break;
             }
         }
